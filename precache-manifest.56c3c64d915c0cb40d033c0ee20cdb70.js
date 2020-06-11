2e57108ea6639139e6ca8608fdd555fb4223ae49
Sonia Kandah <sonia.kandah@gmail.com>
2020-06-10 20:28:32 -0700

    Updates

---
+++ precache-manifest.56c3c64d915c0cb40d033c0ee20cdb70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4de2cfbb10e686984d48e79ca197979",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "b095579c4286d22a17d0",
+    "url": "/my-react-app/static/js/2.70e2b4e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-app/static/js/2.70e2b4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32623a21ca7c39dd34b",
+    "url": "/my-react-app/static/js/main.9a3a9e13.chunk.js"
+  },
+  {
+    "revision": "7a90a31edd55a0925c68",
+    "url": "/my-react-app/static/js/runtime-main.9aa88097.js"
+  }
+]);
